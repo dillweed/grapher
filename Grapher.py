6c02002e32e172c9c72b3cf4b7982d7ca8ab5ae1
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-23 11:04:42 -0800

    Updated docstrings

--- Grapher.py
+++ Grapher.py
@@ -1,15 +1,15 @@
-"""Generate a 2d graph object for displaying (x,y) data"""
+"""Generate a 2d graph object for displaying (x,y) data as text"""
 
 
 class Grapher:
-    """Generate a 2d graph object for displaying (x,y) data
+    """Generate a 2d graph object for displaying (x,y) data as text.
 
     Args:
-    height (int): Number of vertical positions
-    width (int): Number of horizontal positions
-    init_x (int): 0 reference offset from left edge 
-    init_y (int): 0 reference offset from bottom edge
-    fill (str): Character to fill each position in grid. Default is "." 
+        height (int): Number of vertical positions.
+        width (int): Number of horizontal positions.
+        init_x (int): Optional. 0 reference offset from left edge.
+        init_y (int): Optional. 0 reference offset from bottom edge.
+        fill (str): Optional. Character to fill each position in grid. Default is ".". 
     """
 
     def __init__(self, height=10, width=10, init_x=0, init_y=0, fill="."):
@@ -18,67 +18,66 @@ class Grapher:
         self.init_x = init_x
         self.init_y = init_y
         self.fill = fill
-        self.graph = self.generate()
+        self.graph = self._generate_graph()
 
-    def generate(self) -> list[list[str]]:
-        """Generate a graph sized from init arguments.
+    def _generate_graph(self) -> list[list[str]]:
+        """Generate a graph with the specified dimensions.
 
         Returns:
-            list[list[str]]: List of rows of positions
+            list[list[str]]: A list of rows of positions in the graph.
         """
-        block = [[self.fill for _ in range(self.width)]
-                 for _ in range(self.height)]
-        return block
+        return [[self.fill for _ in range(self.width)]
+                for _ in range(self.height)]
 
-    def set(self, _x, _y, item):
+    def set_value(self, x, y, value):
         """Set value at a specified position to item, usually str or int.
 
         Args:
-            _x (int): x coordinate to set
-            _y (int): y coordinate to set
-            item (str or int): Value, usually str or int, to set at position
+            x (int): Horizontal coordinate to set.
+            y (int): Vertical coordinate to set.
+            item (str or int): Value, usually str or int, to set at position.
         """
-        _x, _y = self.transpose(_x, _y)
-        self.graph[_x][_y] = item
+        x, y = self._transpose(x, y)
+        self.graph[x][y] = value
 
-    def get(self, _x, _y):
+    def get_value(self, x, y):
         """Get value at position.
 
         Args:
-            _x (int): x coordinate to get
-            _y (int): y coordinate to get
+            x (int): Horizontal coordinate to get.
+            y (int): Vertical coordinate to get.
 
         Returns:
-            str or int: Value of object from specified position
+            str or int: Value of object from specified position.
         """
-        _x, _y = self.transpose(_x, _y)
-        return self.graph[_x][_y]
+        x, y = self._transpose(x, y)
+        return self.graph[x][y]
 
-    def transpose(self, _x, _y) -> tuple:
-        """Converts x,y coordinates to i,j type tuple for use in a subscriptable
+    def _transpose(self, x, y) -> tuple:
+        """Convert x,y coordinates to i,j type tuple for use in a subscriptable
         nested loop. (list of lists) 
 
-        x values increase to the right like nested j values do. 
-        y values increase upwards which is opposite to i in general.
-        x,y starting position is lower-left by default. i,j is upper-left.
+        X values increase to the right like nested j values do. 
+        X values increase upwards which is opposite to i in general.
+        The x,y starting position is lower-left by default. i,j is upper-left.
 
         Args:
-            _x (int): x coordinate
-            _y (int): y coordinate
+            x (int): Horizontal coordinate to convert.
+            y (int): Vertical coordinate to convert.
 
         Returns:
             tuple: (i,j) Position reference for use in a subscriptable nested loop. 
         """
         return (
-            [n for n in range(self.height)][::-1][_y + self.init_y],
-            _x + self.init_x,
+            [n for n in range(self.height)][::-1][y + self.init_y],
+            x + self.init_x,
         )
 
     def display(self) -> str:
-        """Converts graph list to printable string.
+        """Convert graph list to printable string.
 
         Returns:
-            str: Easily printable block string
+            str: An easily printable block string.
         """
         # Convert any non-str items to str
         graph_list = [[str(item) for item in line] for line in self.graph]
