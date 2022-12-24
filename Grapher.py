546dda049c839589bd2cf27f8343ea34ccfbe7c1
Dill <4053467+dillweed@users.noreply.github.com>
2022-12-24 09:26:26 -0800

    Updated transpose algo.

--- Grapher.py
+++ Grapher.py
@@ -1,4 +1,7 @@
 """Generate a 2d graph object for displaying (x,y) data as text"""
+# TODO fix _transpose algo. Use (max_value - (input_number - 1))
+# to invert the value instead of a reversed list comprehension.
+# TODO catch init_x and init_y values that are out of range.
 
 
 class Grapher:
@@ -9,7 +12,7 @@ class Grapher:
         width (int): Number of horizontal positions.
         init_x (int): Optional. 0 reference offset from left edge.
         init_y (int): Optional. 0 reference offset from bottom edge.
-        fill (str): Optional. Character to fill each position in grid. Default is ".". 
+        fill (str): Optional. Character to fill each position in grid. Default is ".".
     """
 
     def __init__(self, height=10, width=10, init_x=0, init_y=0, fill="."):
@@ -26,8 +29,7 @@ class Grapher:
         Returns:
             list[list[str]]: A list of rows of positions in the graph.
         """
-        return [[self.fill for _ in range(self.width)]
-                for _ in range(self.height)]
+        return [[self.fill for _ in range(self.width)] for _ in range(self.height)]
 
     def set_value(self, x, y, value):
         """Set value at a specified position to item, usually str or int.
@@ -37,8 +39,8 @@ class Grapher:
             y (int): Vertical coordinate to set.
             item (str or int): Value, usually str or int, to set at position.
         """
-        x, y = self._transpose(x, y)
-        self.graph[x][y] = value
+        i, j = self._transpose(x, y)
+        self.graph[i][j] = value
 
     def get_value(self, x, y):
         """Get value at position.
@@ -50,14 +52,14 @@ class Grapher:
         Returns:
             str or int: Value of object from specified position.
         """
-        x, y = self._transpose(x, y)
-        return self.graph[x][y]
+        i, j = self._transpose(x, y)
+        return self.graph[i][j]
 
     def _transpose(self, x, y) -> tuple:
         """Convert x,y coordinates to i,j type tuple for use in a subscriptable
-        nested loop. (list of lists) 
+        nested loop. (list of lists)
 
-        X values increase to the right like nested j values do. 
+        X values increase to the right like nested j values do.
         X values increase upwards which is opposite to i in general.
         The x,y starting position is lower-left by default. i,j is upper-left.
 
@@ -66,10 +68,22 @@ class Grapher:
             y (int): Vertical coordinate to convert.
 
         Returns:
-            tuple: (i,j) Position reference for use in a subscriptable nested loop. 
+            tuple: (i,j) Position reference for use in a subscriptable nested loop.
         """
+        # Previous algo was inefficient. Used reversed list to invert y->i.
+
+        # return (
+        #     [n for n in range(self.height)][::-1][y + self.init_y],
+        #     x + self.init_x,
+        # )
+
+        # Current algo uses a cool math trick to invert a number within a range.
+        # Without 0 indexing, (max number of range) - (number to invert - 1)
+        # will invert a number within a range. Range (1 through 6) input 2 outputs 5.
+        # With 0 indexing, use (max number of range - 1) - (number to invert).
+        # Range (0 through 5) input 1 outputs 4.
         return (
-            [n for n in range(self.height)][::-1][y + self.init_y],
+            self.height - 1 - (y + self.init_y),
             x + self.init_x,
         )
 
